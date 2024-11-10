9ba593fffb4a594293ea48cebcf54a9f22b86d91
Blake Eriks <blakeeriks.dev@gmail.com>
2024-11-10 15:47:28 -0500

    support supplying telegram database id for a user

--- lib/telegram/commands/habits/index.ts
+++ lib/telegram/commands/habits/index.ts
@@ -4,17 +4,26 @@ import type { HabitCommand, HabitContext } from '../../types'
 import { enterScene } from '@/lib/util/telegraf'
 import listHabits from './listHabits'
 import logHabitScene, { LOG_HABIT_SCENE } from './logHabit'
+import logHabitsScene, { LOG_HABITS_SCENE } from './logHabits'
 import { NEW_HABIT_SCENE, newHabitScene } from './newHabit'
 import removeHabitScene, { REMOVE_HABIT_SCENE } from './removeHabit'
+import setDatabaseIdScene, { SET_DATABASE_ID_SCENE } from './setDatabaseId'
 import habitSummary from './summarizeHabits'
 
 export const HABIT_SCENES: BaseScene<HabitContext>[] = [
+  setDatabaseIdScene,
   newHabitScene,
   removeHabitScene,
   logHabitScene,
+  logHabitsScene,
 ]
 
 export const HABIT_COMMANDS: HabitCommand[] = [
+  {
+    name: 'set_database_id',
+    description: 'Set your Notion Habit Database ID',
+    action: enterScene(SET_DATABASE_ID_SCENE),
+  },
   {
     name: 'list_habits',
     description: 'List the habits you are tracking',
@@ -32,6 +41,11 @@ export const HABIT_COMMANDS: HabitCommand[] = [
   },
   {
     name: 'log_habits',
+    description: 'Log your habit data for today',
+    action: enterScene(LOG_HABITS_SCENE),
+  },
+  {
+    name: 'log_habit',
     description: 'Log your habit data for today',
     action: enterScene(LOG_HABIT_SCENE),
   },
