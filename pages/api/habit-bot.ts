64f1ff90f4d0954fe5b88b9a3effdbea94365dbb
Blake Eriks <blakeeriks.dev@gmail.com>
2024-11-27 21:43:45 -0300

    update reponse code

--- pages/api/habit-bot.ts
+++ pages/api/habit-bot.ts
@@ -4,7 +4,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     if (req.method !== 'POST') {
-      return res.status(405).json({ message: 'This endpoint only accepts POST requests' })
+      return res.status(200).json({ message: 'Habit Bot Webhook is ready!' })
     }
     await habitBot.handleUpdate(req.body)
     return res.status(200).json({ message: 'Success' })
